4d06c4f4051d247c11f9bd4d43d4974237a821fd
Jeff Segal <jeffrey.segal@gmail.com>
2016-03-04 18:25:37 -0500

    Deleting *-compiled.js files and adding them to .gitignore

--- client/app/app.constant-compiled.js
+++
@@ -1,7 +0,0 @@
-(function (angular, undefined) {
-  'use strict';
-
-  angular.module('legitrackApp.constants', []).constant('appConfig', { userRoles: ['guest', 'user', 'admin'] });
-})(angular);
-
-//# sourceMappingURL=app.constant-compiled.js.map
--- client/app/limsService/limsService.service-compiled-compiled.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('legitrackApp').service('limsService', function () {
-  // AngularJS will instantiate a singleton by calling "new" on this function
-});
-
-//# sourceMappingURL=limsService.service-compiled.js.map
-
-//# sourceMappingURL=limsService.service-compiled-compiled.js.map
